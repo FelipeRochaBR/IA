646ec59231b4fee84ad233773f54d1e807fd1282
Pedro <pedrohjs_vargas@hotmail.com>
2025-03-18 13:32:59 -0300

    Codigo do prof add

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask 
+import backEnd.mainGrid as principal
 
 app = Flask(__name__)
 
 from views import *
+
+#def teste() :
+ #  print('',principal.caminho) 
 
 if __name__ == "__main__":
     app.run()
