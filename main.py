f71b105607287c065aca039de881596b226a95b0
Pedro <pedrohjs_vargas@hotmail.com>
2025-03-23 11:21:49 -0300

    Código do prof atualizado

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, request, render_template, url_for, redirect
-
+from flask import Flask ,request
+from backEnd import mainGrid 
 app = Flask(__name__)
 
 from views import *
 
+def test():
+    print ('',mainGrid())
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=7000)
+
+ 
