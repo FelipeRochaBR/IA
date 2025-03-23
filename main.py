fc7411488d9954039ab1400a817a1a09f97ab7c0
FelipeRochaBR <felipepiresrochabackup@gmail.com>
2025-03-23 13:45:00 -0300

    Inserção das variáveis no arquivo views.py e comunicação entre essas variáveis com o arquivo homepage.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask ,request
-from backEnd import mainGrid 
+# from backEnd import mainGrid 
 app = Flask(__name__)
 
 from views import *
 
-def test():
-    print ('',mainGrid())
-
+# def test():
+#     print ('',mainGrid())
 
 if __name__ == "__main__":
     app.run(debug=True,port=7000)
--- views.py
+++ views.py
@@ -1,7 +1,39 @@
 from main import app
-from flask import render_template
+from flask import render_template, request, redirect
 
 # rotas
 @app.route("/")
 def Gridpage():
     return render_template("homepage.html")
+
+# rotas
+@app.route("/", methods=['GET','POST'])
+def resposta():
+    if request.method == 'POST':
+        inicioX = request.form.get("inicioX")
+        inicioY = request.form.get("inicioY")
+        objetivoX = request.form.get("objetivoX")
+        objetivoY = request.form.get("objetivoY")
+        obstaculos = request.form.get("obstaculos")
+        metodo = request.form.get("metodo")
+
+        if inicioX:  
+            try:
+                inicioX = int(inicioX)
+                inicioY = int(inicioY)
+                objetivoX = int(objetivoX) 
+                objetivoY = int(objetivoY) 
+                obstaculos = int(obstaculos) 
+                metodo = str(metodo)
+                print(f"Valor recebido: {inicioX} {inicioY} {objetivoX} {objetivoY} {obstaculos} {metodo}")  
+                return redirect("/") 
+            except ValueError:
+                return "Erro: O valor inserido não é um número válido."
+        else:
+            return "Erro: O campo 'inicio' não foi preenchido."
+        
+        
+         
+    else: 
+        return render_template("homepage.html")
+        
